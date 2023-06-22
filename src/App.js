26e1391e88712cbd06feb98b7edae81d8f863bd4
Druv <druvkotwani12@gmail.com>
2023-06-22 17:39:41 +0530

    Styles done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <header class="header">
+      <div class="logo">
+        <img src="v1/logo.png" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+      <button class="btn btn-large btn-open">Share a fact</button>
+    </header>
+  );
+}
+
+export default App;
